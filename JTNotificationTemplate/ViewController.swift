bada2d5ad1e0347e00814360d7f05dbb2e0d00d2
Jiayi Liu <liujiayi19920724@gmail.com>
2019-01-28 10:45:56 +1300

    fire some testing notification when user press the central button once permission is granted

--- JTNotificationTemplate/ViewController.swift
+++ JTNotificationTemplate/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
             if(settings.authorizationStatus != .authorized){
                 self.requestPermission()
             }else{
-                // fire some testing notification
+                self.fireTestNotification()
             }
         }
     }
@@ -61,5 +61,29 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
     }
     
+    func fireTestNotification(){
+        // create notification
+        let content = UNMutableNotificationContent()
+        content.title = "Title Here"
+        content.body = "Body Here"
+        content.sound = UNNotificationSound.default
+        
+        // after user enters, how long to trigger the notification
+        let timeInSeconds : TimeInterval = 5
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInSeconds,
+                                                        repeats: false)
+        
+        // send notification request
+        let request = UNNotificationRequest(identifier: "joytest",
+                                            content: content,
+                                            trigger: trigger)
+        
+        // trying to add the notification request to notification center
+        notificationCenter.add(request, withCompletionHandler: { (error) in
+            if error != nil {
+                print("Error adding notification with identifier : joytest")
+            }
+        })
+    }
 }
 
