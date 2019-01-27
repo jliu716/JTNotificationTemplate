8fb648965cb4e697c02ee3d1c9e20438f3788944
Jiayi Liu <liujiayi19920724@gmail.com>
2019-01-28 10:41:42 +1300

    programmatically add a  central button

--- JTNotificationTemplate/ViewController.swift
+++ JTNotificationTemplate/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let button : UIButton = UIButton.init(frame: CGRect.zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.setupButton()
     }
+    
+    func setupButton(){
+        // draw a button take up the whole middle space
+        let height = UIScreen.main.bounds.size.height / 3.0
+        let width  = UIScreen.main.bounds.size.width
+        let x = CGFloat(0)
+        let y = UIScreen.main.bounds.size.height / 3.0
+        let frame : CGRect = CGRect(x: x, y: y, width: width, height: height)
+        
+        button.frame = frame
+        button.backgroundColor = UIColor.groupTableViewBackground
+        button.setTitle("PRESS ME", for: UIControl.State.normal)
+        button.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        button.setTitleColor(UIColor.gray, for: UIControl.State.highlighted)
+        
+        self.view.addSubview(button)
+        button.addTarget(self, action: #selector(checkPermission), for: UIControl.Event.touchUpInside)
+        
+    }
+    
+    @objc func checkPermission(){
 
-
+    }
+    
 }
 
