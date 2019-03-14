bb1817fdb680cc6fb51d97046ecc95fe89319b88
Jiayi Liu <liujiayi19920724@gmail.com>
2019-03-14 18:58:26 +1300

    handle notification within app delegate

--- JTNotificationTemplate/AppDelegate.swift
+++ JTNotificationTemplate/AppDelegate.swift
@@ -7,40 +7,52 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
-
+    let notificationCenter : UNUserNotificationCenter = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        notificationCenter.delegate = self
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        fireTestNotification()
+        
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    func fireTestNotification(){
+        // create notification
+        let content = UNMutableNotificationContent()
+        content.title = "Title Here"
+        content.body = "Body Here"
+        content.sound = UNNotificationSound.default
+        
+        // after user enters, how long to trigger the notification
+        let timeInSeconds : TimeInterval = 5
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInSeconds,
+                                                        repeats: false)
+        
+        // send notification request
+        let request = UNNotificationRequest(identifier: "joytest",
+                                            content: content,
+                                            trigger: trigger)
+        
+        // trying to add the notification request to notification center
+        notificationCenter.add(request, withCompletionHandler: { (error) in
+            if error != nil {
+                print("Error adding notification with identifier : joytest")
+            }
+        })
     }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
--- JTNotificationTemplate/ViewController.swift
+++ JTNotificationTemplate/ViewController.swift
@@ -43,8 +43,6 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         notificationCenter.getNotificationSettings { (settings) in
             if(settings.authorizationStatus != .authorized){
                 self.requestPermission()
-            }else{
-                self.fireTestNotification()
             }
         }
     }
@@ -61,29 +59,6 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         }
     }
     
-    func fireTestNotification(){
-        // create notification
-        let content = UNMutableNotificationContent()
-        content.title = "Title Here"
-        content.body = "Body Here"
-        content.sound = UNNotificationSound.default
-        
-        // after user enters, how long to trigger the notification
-        let timeInSeconds : TimeInterval = 5
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInSeconds,
-                                                        repeats: false)
-        
-        // send notification request
-        let request = UNNotificationRequest(identifier: "joytest",
-                                            content: content,
-                                            trigger: trigger)
-        
-        // trying to add the notification request to notification center
-        notificationCenter.add(request, withCompletionHandler: { (error) in
-            if error != nil {
-                print("Error adding notification with identifier : joytest")
-            }
-        })
-    }
+    
 }
 
